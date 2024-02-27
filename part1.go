ecde7a22bd95a6820b389f5d4baeb10cc0bc4eb1
Ekaterina Batulina <katya_batulina@mail.ru>
2024-02-27 22:22:33 +0300

    Part 1

---
+++ part1.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	// "strconv"
+)
+
+// import "os"
+
+func askForInput() (string, error) {
+	var input string
+	fmt.Println("Enter string: ")
+	fmt.Scan(&input)
+	if len(input) > 100 {
+		return "", errors.New("Input is too long")
+	}
+	return input, nil
+}
+
+func main() {
+	input, err := askForInput()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// rune_slice := []rune(input)
+	char_slice := make([]string, 100)
+	for _, ch := range input {
+		char_slice = append(char_slice, string(ch))
+	}
+	fmt.Println(char_slice)
+	return
+}
